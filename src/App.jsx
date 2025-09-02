cbee3800efd238dccf48f07e59f97c72fd942b89
lucasmaloni <l.maloni.c@gmail.com>
2025-09-02 20:29:43 -0300

    reading and rendering data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from 'axios';
 
 function App() {
   const[tasks, setTasks] = useState([]);
   
+  useEffect(()=> {
+    const getTasks = async () => {
+      try{
+        const response = await axios.get("http://localhost:3001/tasks");
+        setTasks(response.data);
+      } catch(error){
+        console.log("There was an error  trying to retrieve the data", error);
+      }
+    }
+
+    getTasks();
+  },[])
+
+  function enlistTasks(){
+      const taskList = tasks.map(task => <li key={task.id}>{task.title} - {task.description}</li>)
+    
+    return <ul>{taskList}</ul>
+  }
+
   return (
     <div>
       <h1>to-do app</h1>
+      {enlistTasks()}
     </div>
   );
 }
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function TaskForm(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+}
