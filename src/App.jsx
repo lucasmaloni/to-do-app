7784bde4f88094ec6af809fbfd815e2b113f4ee4
lucasmaloni <l.maloni.c@gmail.com>
2025-09-02 22:12:05 -0300

    input and insertion in db working
    need to open new spaces for input to add details for other objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from 'axios';
+import TaskForm from "./components/TaskForm";
 
 function App() {
   const[tasks, setTasks] = useState([]);
@@ -24,9 +25,20 @@ function App() {
     return <ul>{taskList}</ul>
   }
 
+  const addTask = async(title) => {
+    try{
+      const newTask = {title: title, description:"", status:"pending"};
+      const response = await axios.post('http://localhost:3001/tasks', newTask);
+      setTasks([...tasks, response.data]);
+    } catch(error){
+      console.error("Failed to add task:", error);
+    }
+  }
+
   return (
     <div>
       <h1>to-do app</h1>
+      <TaskForm onTaskCreate={addTask}/>
       {enlistTasks()}
     </div>
   );
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,32 @@
 import { useState } from "react";
 
-function TaskForm(){
+function TaskForm({ onTaskCreate }){
     const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+    /*const [description, setDescription] = useState('');*/
 
+    const handleSubmit = (submit) =>{
+        submit.preventDefault();
+
+        if (!title.trim()) return;
+
+        onTaskCreate(title);
+
+        setTitle('')
+    }
+    
+    return(
+        <div>
+            <h2> Add a new Task </h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder="Task Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <button type="submit">Add Task</button>
+            </form>
+        </div>
+    );
 }
+
+export default TaskForm;
